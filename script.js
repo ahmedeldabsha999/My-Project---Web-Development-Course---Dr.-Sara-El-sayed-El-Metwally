f3389bc9d02248cf4e3c828dcea29a6899cd0c39
Ahmed Eldabsha <84291116+ahmedeldabsha999@users.noreply.github.com>
2021-05-27 17:21:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+onload = document.getElementById('date').innerHTML += Date();
+document.getElementById('date').style.color = "black";
+document.getElementById('date').style.fontWeight = "lighter";
+document.getElementById('date').style.fontStyle = "italic";
+document.getElementById('date').style.fontFamily = "sans-serif";
+document.getElementById('date').style.fontSize = "14.5px";
